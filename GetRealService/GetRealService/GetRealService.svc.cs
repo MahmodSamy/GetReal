7bc04d10f5c9d54c778547b2c59aaa8c4f66e058
MahmodSamy <ma7mod_samy2006@yahoo.com>
2014-06-09 19:14:10 +0300

    test

--- GetRealService/GetRealService/GetRealService.svc.cs
+++ GetRealService/GetRealService/GetRealService.svc.cs
@@ -14,7 +14,7 @@ namespace GetRealService
     {
         public string GetData(int value)
         {
-            return string.Format("You entered: {0}", value);
+            return string.Format("You entered: {0}", value); 
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
